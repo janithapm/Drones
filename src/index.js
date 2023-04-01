fa779ded2294d468eba0ac3eafe92fe54f38dd98
Janitha Madushanka <janitha.madushanka@circles.asia>
2023-04-01 13:37:58 +0530

    added missing packages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express, { json, urlencoded } from 'express';
-import cors from 'cors';
+const express = require('express')
+const cors = require('cors')
 
 const app = express();
 
 app.use(cors());
-app.use(json());
-app.use(urlencoded( {extended: true} ));
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
 
 const port = process.env.PORT || 8080;
 
