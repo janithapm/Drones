fd86f04e67c626e7334edce1fd368d7ce1642efb
Janitha Madushanka <janitha.madushanka@circles.asia>
2023-04-02 22:41:10 +0530

    updated log time interval

--- src/scheduler/scheduler.js
+++ src/scheduler/scheduler.js
@@ -14,16 +14,6 @@ function createLog() {
     const logStream = fs.createWriteStream(fileName, { flags: 'a' });
     logStream.write(`\ntime:${moment().format('YYYY-MM-DD-hh:mm:ss')}\n`);
 
-    // const query = `SELECT serial as SERIAL,battery as BATTERY FROM drones`;
-    // db.each(query, (err, row) => {
-    //     if (err) {
-    //         console.error(err.message);
-    //         throw err;
-    //     } else {
-    //         logStream.write(`${JSON.stringify(row)}\n`);
-    //     }
-    // });
-
     Drone.getBatteryLog().then(battery => {
         logStream.write(`${JSON.stringify(battery)}\n`);
     }).catch(err =>  {
@@ -35,10 +25,12 @@ function createLog() {
     });
 }
 
+let timeInterval =  process.env.LOG_INTERVAL || 30000;
+
 // write log in to logs folder for every 30 seconds
 function createLogInInterval() {
     setInterval(() => {
         createLog()
-    }, 600000);
+    }, timeInterval);
 }
 module.exports = createLogInInterval();
